5c5e205442e25fbcdee138756799b29018024309
IHaveAbs <109310420+IHaveAbs@users.noreply.github.com>
2022-12-12 20:00:59 -0800

    Update BovineGenomics.cpp

--- Bronze/BasicCompleteSearch/BovineGenomics.cpp
+++ Bronze/BasicCompleteSearch/BovineGenomics.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    cin.tie(nullptr);
 
 	freopen("cownomics.in", "r", stdin);
 	freopen("cownomics.out", "w", stdout);
