f695ec7e381efa1381a030a024b5e4c7c50221e9
IHaveAbs <109310420+IHaveAbs@users.noreply.github.com>
2022-12-12 20:05:52 -0800

    Update WhyDidTheCowCrossTheRoad3.cpp

--- Bronze/IntroductionToSorting/WhyDidTheCowCrossTheRoad3.cpp
+++ Bronze/IntroductionToSorting/WhyDidTheCowCrossTheRoad3.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <cstdio>
+#include <algorithm>
 using namespace std;
 
+
 int main() {
+    
     ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+    
+    freopen("cowqueue.in", "r", stdin);
+    freopen("cowqueue.out", "w", stdout);
 
-	freopen("gymnastics.in", "r", stdin);
-	freopen("gymnastics.out", "w", stdout);
-	
-    int grid[21][21] = {0};
-    int k, n;
+    int n;
+    cin>>n;
     
-    cin>>k>>n;
     
-    vector<vector<int>> arry;
+    vector<pair<int, int>> arry;
     
-    for (int i = 0; i<k; i++) {
-        vector<int> temp;
-        for (int j = 0; j<n; j++) {
-            int a;
-            cin>>a;
-            temp.push_back(a);
-        }
+    
+    for (int i = 0; i<n; i++) {
+        int a, b;
+        cin>>a>>b;
+        
+        pair<int, int> temp(a, b);
+        
         arry.push_back(temp);
     }
     
-    for (int i = 0; i<k; i++) {
-        for (int j= 0; j<n; j++) {
-            for (int x = j+1; x<n; x++) {
-                grid[arry[i][j]][arry[i][x]]++;
-            }
+    sort(arry.begin(), arry.end());
+    
+    int tpos = arry[0].first+arry[0].second;
+    
+    for (int i = 1; i<n; i++) {
+        if (tpos < arry[i].first) {
+            tpos = arry[i].first;
         }
+        tpos+=arry[i].second;
     }
     
-    int ans = 0;
-    for (int i = 1; i<=20; i++) {
-        for (int j = 1; j<=20; j++) {
-            if (grid[i][j] == k) {
-                ans++;
-            }
-        }
-    }
-    cout<<ans<<endl;
+    cout<<tpos<<"\n";
     
-    return 0;
 }
