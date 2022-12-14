36bf8fc35b2893d0205a66816c9501d4b0e47648
IHaveAbs <109310420+IHaveAbs@users.noreply.github.com>
2022-12-13 21:57:00 -0800

    Update DaisyChains.cpp

--- Bronze/BasicCompleteSearch/DaisyChains.cpp
+++ Bronze/BasicCompleteSearch/DaisyChains.cpp
@@ -9,33 +9,29 @@ int main() {
     
     ios_base::sync_with_stdio(0); cin.tie(0);
 	 
-	int n;
-	cin>>n;
+    int n;
+    cin>>n;
 	
 	
-	vector<int> arry(n+1);
-	for (int i = 1; i<=n; i++) {
-	    cin>>arry[i];
-	}
+    vector<int> arry(n+1);
+    for (int i = 1; i<=n; i++) {
+	cin>>arry[i];
+    }
 	
-	int ans = 0;
-	for (int i = 1; i<=n; i++) {
-	    int sum = 0;
-	    for (int j = i; j<=n; j++) {
-	        sum += arry[j];
-	        int average = sum/(j-i+1);
-	        for (int x = i; x<=(j); x++) {
-	            if (sum == arry[x]*(j-i+1)) {
-	                ans++;
-	                break;
-	            }
+    int ans = 0;
+    for (int i = 1; i<=n; i++) {
+        int sum = 0;
+	for (int j = i; j<=n; j++) {
+	    sum += arry[j];
+	    int average = sum/(j-i+1);
+	    for (int x = i; x<=(j); x++) {
+	        if (sum == arry[x]*(j-i+1)) {
+	            ans++;
+	            break;
 	        }
 	    }
 	}
+    }
 	
-	cout<<ans<<endl;
-	
-    
-    
-    
+    cout<<ans<<endl;
 }
