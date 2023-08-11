ebd2f39114a3d83e8497423c9acc1214de91c309
Situ Zhao <109310420+IHaveAbs@users.noreply.github.com>
2023-08-10 22:39:04 -0700

    Update StaticRangeSum.cpp

--- Silver/PrefixSums/StaticRangeSum.cpp
+++ Silver/PrefixSums/StaticRangeSum.cpp
@@ -1,29 +1,50 @@
-#include <iostream>
+//
+//  main.cpp
+//  MAIN
+//
+//  Created by Situ Zhao on 8/10/23.
+//
 
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+
+#define ll long long
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define vi vector<int>
+#define pi pair<int, int>
 using namespace std;
 
-long long arry[500001];
+ll arr[500001] = {0};
 
-int main()
-{
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    
     int n, q;
     cin>>n>>q;
     
-    
-    long long temp;
-    cin>>temp;
-    arry[0] = temp;
-    for (int i = 1; i<n; i++) {
-        cin>>temp;
-        arry[i] = temp+arry[i-1];
+    for (int i = 1; i<=n; i++) {
+        cin>>arr[i];
+        arr[i] = arr[i]+arr[i-1];
     }
     
-    for (int i = 0; i<q; i++) {
-        int l, r;
-        cin>>l>>r;
-        
-        cout<<arry[r-1]-arry[l-1]<<endl;
+    int a, b;
+    for (int i = 1; i<=q; i++) {
+        cin>>a>>b;
+        cout<<arr[b]-arr[a]<<endl;
     }
-
+    
     return 0;
 }
