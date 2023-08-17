45ea696208f24944860e860f92331a6db625de9b
Situ Zhao <109310420+IHaveAbs@users.noreply.github.com>
2023-08-17 10:20:28 -0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+#define endl "\n"
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+  
+  
+    return 0;
+}
