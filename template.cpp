84cdd32c15913b00820478e400b28a34b2adf608
Situ Zhao <109310420+IHaveAbs@users.noreply.github.com>
2023-09-25 07:31:09 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <deque>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define endl "\n"
@@ -15,5 +7,7 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
+    
+    
     return 0;
 }
